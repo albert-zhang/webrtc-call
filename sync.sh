ae7f07264c847a415a8258abff97f1fa14ec42f9
Albert Zhang <albertzhang.pro@gmail.com>
2019-02-13 18:10:46 +0800

    up

--- sync.sh
+++ sync.sh
@@ -1,6 +1,7 @@
 #scp -r `pwd` mxj@10.28.3.191:/home/mxj/work
 
 rsync -rav -e 'ssh -p 28625' \
+  --exclude '.DS_Store' \
   --exclude 'node_modules' \
   --exclude '.git' \
-  . root@144.34.206.188:/work/study-webrtc
+  . root@144.34.206.188:/work/webrtc-call
